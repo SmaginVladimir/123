2db70b7216384d1d3e11163bb0a2e47c69dd3983
Popov Evgeny <eugene.eva.20.@gmail.com>
2023-04-27 18:16:16 +0600

    Added routing. Created Layout, Home and Error page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css'
+import Layout from "./ui/Layout/Layout";
+import {Route, Routes} from "react-router-dom";
+import Home from "./features/pages/Home";
+import Error from "./ui/Error/Error";
 
 const App = () => (
-    <div>
-        Lazy-tracker!
-    </div>
+    <Routes>
+        <Route path={'/'} element={<Layout/>}>
+            <Route index element={<Home />} />
+        </Route>
+        <Route path={'*'} element={<Error />} />
+    </Routes>
 );
 
 export default App
