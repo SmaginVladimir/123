b5ab5270ec7bbc292860c22f9c7bc1e677605856
Popov Evgeny <eugene.eva.20.@gmail.com>
2023-04-29 18:39:25 +0600

    Change styles in Error component.

--- frontend/src/ui/Error/Error.tsx
+++ frontend/src/ui/Error/Error.tsx
@@ -2,7 +2,7 @@ import {NavLink} from "react-router-dom";
 
 const Error = () => {
     return (
-        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
             <div className="text-center text-red">
                 <p className="text-base font-semibold">404</p>
                 <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Page not found</h1>
